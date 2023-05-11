58a99acaf19a760866c725cae1330732d3d9f821
BoredomX <sukapidor67@gmail.com>
2023-05-11 17:49:41 +0300

    diagnoissi

--- assets/preload/data/mnalk/drain.lua
+++ assets/preload/data/mnalk/drain.lua
@@ -1,7 +1,3 @@
-function onCreate()
-    setProperty('gf.visible', false)
-end
-
 function opponentNoteHit()
     if getProperty('health') > 0.2 then
         setProperty('health', getProperty('health') - 0.0085);

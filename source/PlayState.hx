78dc0c9d9195629455101162962e8f86d3accf43
Nintbros <135995010+Nintbros@users.noreply.github.com>
2023-10-09 18:01:09 +0200

    do something actually useful for once
    scale down icons if in 3zloty (watch as it proceeds not to work at all

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2252,11 +2252,11 @@ class PlayState extends MusicBeatState
 		// FlxG.watch.addQuick('VOL', vocals.amplitudeLeft);
 		// FlxG.watch.addQuick('VOLRight', vocals.amplitudeRight);
 
-		var mult:Float = FlxMath.lerp(1, iconP1.scale.x, CoolUtil.boundTo(1 - (elapsed * 9 * playbackRate), 0, 1));
+		var mult:Float = FlxMath.lerp((SONG.song == "3zloty" ? 0.8 : 1), iconP1.scale.x, CoolUtil.boundTo(1 - (elapsed * 9 * playbackRate), 0, 1));
 		iconP1.scale.set(mult, mult);
 		iconP1.updateHitbox();
 
-		var mult:Float = FlxMath.lerp(1, iconP2.scale.x, CoolUtil.boundTo(1 - (elapsed * 9 * playbackRate), 0, 1));
+		var mult:Float = FlxMath.lerp((SONG.song == "3zloty" ? 0.8 : 1), iconP2.scale.x, CoolUtil.boundTo(1 - (elapsed * 9 * playbackRate), 0, 1));
 		iconP2.scale.set(mult, mult);
 		iconP2.updateHitbox();
 
@@ -4155,8 +4155,7 @@ class PlayState extends MusicBeatState
 			notes.sort(FlxSort.byY, ClientPrefs.downScroll ? FlxSort.ASCENDING : FlxSort.DESCENDING);
 		}
 
-		iconP1.scale.set(1.2, 1.2);
-		iconP2.scale.set(1.2, 1.2);
+		iconP1.scale.x = iconP1.scale.y = iconP2.scale.x = iconP2.scale.y = (SONG.song == "3zloty" ? 1 : 1.2);
 
 		iconP1.updateHitbox();
 		iconP2.updateHitbox();

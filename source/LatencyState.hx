d17478f72efcbe68e9be566d7135148f00c3e0a5
Nintbros <135995010+Nintbros@users.noreply.github.com>
2023-10-10 20:43:40 +0200

    im stupid and keep breaking shit

--- source/LatencyState.hx
+++ source/LatencyState.hx
@@ -16,7 +16,7 @@ class LatencyState extends FlxState
 	{
 		FlxG.sound.playMusic(Paths.sound('soundTest'));
 
-		add(noteGrp = new FlxTypedGroup<Note>();
+		add(noteGrp = new FlxTypedGroup<Note>());
 
 		for (i in 0...32)
 		{

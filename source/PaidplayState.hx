075af976da389250213f33f6bfb066088cacf9bc
Nintbros <135995010+Nintbros@users.noreply.github.com>
2023-10-26 19:24:15 +0200

    @ThePlank kill yourself

--- source/PaidplayState.hx
+++ source/PaidplayState.hx
@@ -147,9 +147,7 @@ class PaidplayState extends MusicBeatState
 			songText.setBorderStyle(OUTLINE, 	FlxColor.WHITE, 4, 2);
 
 			var icon:HealthIcon = new HealthIcon(cast (song, SongMetadata).songCharacter);
-			icon.x = songText.text.length * (42 / 2) + 12;
-			// using a FlxGroup is too much fuss!
-			icon.y = songText.height / 2 - icon.height / 2;
+			icon.setPosition(songText.text.length * (42 / 2) + 12, songText.height / 2 - icon.height / 2);
 			icon.flipX = icon.char == "zlibty";
 
 			var idiot:FlxSpriteGroup = new FlxSpriteGroup();
